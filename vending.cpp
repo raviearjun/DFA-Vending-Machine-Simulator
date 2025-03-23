c540b600683472d196de25e4e4532943faf1f868
david <davidneilleenirvinne@mail.ugm.ac.id>
2025-03-23 15:03:50 +0700

    improve wild input handler

--- vending.cpp
+++ vending.cpp
@@ -6,22 +6,19 @@
 using namespace std;
 
 // Harga minuman
-map<char, int> harga = {{'A', 3000}, {'B', 4000}, {'C', 5000}};
+map<char, int> harga =  {{'A', 3000}, {'B', 4000}, {'C', 5000}};
 
 // DFA Transitions
 map<string, map<string, string>> dfa;
 
-void loadDFA(const string &filename)
-{
+void loadDFA(string filename){
     ifstream file(filename);
-    if (!file)
-    {
+    if (!file){
         cerr << "Gagal membuka file DFA!" << endl;
         exit(1);
     }
     string line;
-    while (getline(file, line))
-    {
+    while (getline(file, line)){
         istringstream iss(line);
         string state, input, nextState;
         iss >> state >> input >> nextState;
@@ -30,20 +27,16 @@ void loadDFA(const string &filename)
     file.close();
 }
 
-void printDFAPath(const vector<string> &path)
-{
+void printDFAPath(const vector<string> &path){
     cout << "Lintasan DFA: ";
-    for (size_t i = 0; i < path.size(); i++)
-    {
+    for (size_t i = 0; i < path.size(); i++){
         cout << path[i];
-        if (i < path.size() - 1)
-            cout << " -> ";
+        if (i < path.size() - 1) cout << " -> ";
     }
     cout << endl;
 }
 
-int main()
-{
+int main(){
     loadDFA("vending_dfa.txt");
 
     int saldo = 0;
@@ -51,72 +44,75 @@ int main()
     vector<string> lintasan = {currentState};
     string input;
 
-    while (true)
-    {
-        cout << "Masukkan uang atau beli minuman (1000, 2000, 5000, 10000, A, B, C): ";
+    cout << "Daftar Harga Minuman:\n";
+    for(auto [p, q] : harga){
+      cout << p << ": " << q << '\n';
+    }
+    cout << '\n';
+
+    while (true){
+        cout << "Masukkan uang (1000, 2000, 5000, 10000) atau beli minuman (A, B, C): ";
         cin >> input;
 
-        if (dfa[currentState].find(input) != dfa[currentState].end())
-        {
+        if (dfa[currentState].find(input) != dfa[currentState].end()){
             currentState = dfa[currentState][input];
             lintasan.push_back(currentState);
         }
-        else if (harga.find(input[0]) != harga.end())
-        {
+        else if((int)input.size() == 1 && harga.find(input[0]) != harga.end()){
             // Cek jika input adalah minuman (A, B, C)
             char pilihan = input[0];
-            if (saldo >= harga[pilihan])
-            {
+            if (saldo >= harga[pilihan]){
                 int kembalian = saldo - harga[pilihan];
+                cout << '\n';
                 printDFAPath(lintasan);
                 cout << "Minuman " << pilihan << " dapat dibeli. Status: ACCEPTED." << endl;
-                if (kembalian > 0)
-                {
+                if (kembalian > 0){
                     cout << "Kembalian: " << kembalian << endl;
                 }
                 break;
             }
-            else
-            {
+            else{
                 printDFAPath(lintasan);
                 cout << "Uang tidak cukup. Status: REJECTED." << endl;
                 break;
             }
         }
-        else if (stoi(input) > 10000)
-        {
-            cout << "Uang tidak bisa lebih dari 10000" << endl;
-            continue;
-        }
-        else
-        {
-            cout << "Input tidak valid!" << endl;
-            continue;
-        }
+        else { // barang yang tidak ada atau uang yang tidak ada
+            bool valid_money = 0, valid_product = (int)input.size() == 1 && ('A' <= input[0] && input[0] <= 'C');
+            for(string s : {"1000", "2000", "5000", "10000"}) valid_money |= input == s;
+            bool digit_semua = 1;
+            for(char c : input) digit_semua &= '0' <= c && c <= '9';
 
-        if (input == "1000" || input == "2000" || input == "5000" || input == "10000")
-        {
-            int uang = stoi(input);
-            saldo += uang;
-
-            // Tampilkan minuman yang tersedia
-            if (saldo >= 3000)
-            {
-                cout << "ON: ";
-                bool first = true;
-
-                for (auto &item : harga)
-                {
-                    if (saldo >= item.second)
-                    {
-                        if (!first)
-                            cout << ", ";
-                        cout << "Minuman " << item.first;
-                        first = false;
-                    }
-                }
-                cout << endl;
+            if(digit_semua && !valid_money){
+              cout << "Uang yang anda masukkan tidak valid\n";
+              continue;
             }
+            else if(!digit_semua && !valid_product){
+              cout << "Minuman yang anda pilih tidak tersedia\n";
+              continue;
+            }
+        } 
+
+        if(saldo + stoi(input) > 10000){
+            cout << "Uang yang anda masukkan tidak bisa lebih dari 10000" << endl;
+            continue;
+        }
+        saldo += stoi(input);
+        
+        // Tampilkan minuman yang tersedia
+        if (saldo >= 3000){
+            cout << "ON: ";
+            bool first = true;
+
+            for (auto &item : harga){
+                if (saldo >= item.second){
+                    if (!first)
+                        cout << ", ";
+                    cout << "Minuman " << item.first;
+                    first = false;
+                }
+            }
+            cout << endl;
         }
     }
     return 0;
